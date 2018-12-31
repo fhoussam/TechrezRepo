94f1030c59eb3e7318fb6358e1a031dd38014fe8
BlackShuriken <BlackShuriken@DESKTOP-TB58UTS>
2018-12-31 23:50:15 +0100

    commit to new branch 3

--- Techrez/AngularClient/ClientApp/src/app/app.module.ts
+++ Techrez/AngularClient/ClientApp/src/app/app.module.ts
@@ -19,9 +19,9 @@ export function get_settings(appLoadService: AppLoadService) {
 @NgModule({
   declarations: [
     AppComponent,
-    ProductlistComponent
-    ,ProductdetailsComponent
-    ,CategoryPipePipe,
+    ProductlistComponent,
+    ProductdetailsComponent,
+    CategoryPipePipe,
     ProducteditComponent,
   ],
   imports: [
