194cb3b35aa29df5288e78b7365fef3a5a47ee98
Houssam FERTAQ <fertaqhoussam@hotmail.com>
2019-11-23 17:51:43 +0100

    updated angular version + added routing

--- IdentityServer/trone/angularclient/ClientApp/src/app/app.component.spec.ts
+++ IdentityServer/trone/angularclient/ClientApp/src/app/app.component.spec.ts
@@ -1,9 +1,13 @@
 import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
       declarations: [
         AppComponent
       ],
@@ -16,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'anglular'`, () => {
+  it(`should have as title 'angularclient'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('anglular');
+    expect(app.title).toEqual('angularclient');
   });
 
-  it('should render title in a h1 tag', () => {
+  it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to anglular!');
+    expect(compiled.querySelector('.content span').textContent).toContain('angularclient app is running!');
   });
 });
--- IdentityServer/trone/angularclient/ClientApp/src/app/app.module.ts
+++ IdentityServer/trone/angularclient/ClientApp/src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductsComponent as AdminProductsComponent } from './components/admin/products/products.component';
+import { ProductsComponent as TechrezUserProductsComponent } from './components/techrezuser/products/products.component';
+import { UsersComponent } from './components/admin/users/users.component';
+import { OrdersComponent } from './components/techrezuser/orders/orders.component';
+import { Routes, RouterModule } from '@angular/router';
+
+const approutes: Routes = [
+    { path: 'admin/products', component: AdminProductsComponent },
+    { path: '', component: AdminProductsComponent },
+    { path: 'admin/users', component: UsersComponent },
+    { path: 'techrezusers/products', component: TechrezUserProductsComponent },
+    { path: 'techrezusers/orders', component: OrdersComponent },
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        AdminProductsComponent,
+        TechrezUserProductsComponent,
+        UsersComponent,
+        OrdersComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        RouterModule.forRoot(approutes)
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
