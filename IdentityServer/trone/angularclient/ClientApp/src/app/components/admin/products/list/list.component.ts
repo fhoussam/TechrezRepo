7600a64b04fc309800809722c04c4070fab0fd84
Houssam Fertaq <hfertaq@upclear.com>
2019-11-27 12:30:02 +0100

    cleaning some unused code regarding related to last commit

--- IdentityServer/trone/angularclient/ClientApp/src/app/components/admin/products/list/list.component.ts
+++ IdentityServer/trone/angularclient/ClientApp/src/app/components/admin/products/list/list.component.ts
@@ -2,8 +2,6 @@ import { Component, OnInit, Input } from '@angular/core';
 import { adminProductListItem } from '../../../../models/adminProductListItem';
 import { Router } from '@angular/router';
 import { ProductEventEmitterService } from '../../../../services/product-event-emitter.service';
-import { APP_SETTINGS } from '../../../../models/APP_SETTINGS';
-import { category } from '../../../../models/category';
 
 @Component({
     selector: 'list',
@@ -14,11 +12,9 @@ export class ListComponent implements OnInit {
 
     @Input() products: adminProductListItem[];
     selectedItemCode: string;
-    categories: category[];
     constructor(private router: Router, private productEventEmitter: ProductEventEmitterService) {}
 
     ngOnInit() {
-        this.categories = APP_SETTINGS.categories;
     }
 
     selectItem(selectedItem: adminProductListItem) {
