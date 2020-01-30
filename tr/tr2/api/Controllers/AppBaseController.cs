3c5de5e267603c5e352a3b27aefb38536b9bde27
Houssam FERTAQ <fertaqhoussam@hotmail.com>
2020-01-30 18:13:09 +0100

    placing mediator in base controller

---
+++ tr/tr2/api/Controllers/AppBaseController.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AppBaseController : ControllerBase
+    {
+        public IMediator Mediator => HttpContext.RequestServices.GetService<IMediator>();
+    }
+}
--- tr/tr2/api/Controllers/ProductsController.cs
+++ tr/tr2/api/Controllers/ProductsController.cs
@@ -2,38 +2,30 @@
 using app.Operations.Product.Commands.DeleteProduct;
 using app.Operations.Product.Commands.EditProduct;
 using app.Operations.Product.Queries.SearchProduct;
-using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ProductsController : ControllerBase
+    public class ProductsController : AppBaseController
     {
-        private readonly IMediator _mediator;
-
-        public ProductsController(IMediator mediator)
-        {
-            _mediator = mediator;
-        }
-
         [HttpDelete]
         public async Task<IActionResult> Delete(DeleteProductCommand deleteProductCommand)
         {
-            return Ok(await _mediator.Send(deleteProductCommand));
+            return Ok(await Mediator.Send(deleteProductCommand));
         }
 
         [HttpPost]
         public async Task<IActionResult> Post(EditProductCommand editProductCommand) 
         {
-            return Ok(await _mediator.Send(editProductCommand));
+            return Ok(await Mediator.Send(editProductCommand));
         }
 
         [HttpGet]
         public async Task<IActionResult> Get([FromQuery] SearchProductQuery searchProductQuery) 
         {
-            return Ok(await _mediator.Send(searchProductQuery));
+            return Ok(await Mediator.Send(searchProductQuery));
         }
     }
 }
