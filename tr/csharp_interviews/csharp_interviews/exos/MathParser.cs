da35b79cf3fd91f5bafa291c5fc57c96627f58b8
Houssam FERTAQ <fertaqhoussam@hotmail.com>
2020-02-02 23:35:31 +0100

    math parser exo

---
+++ tr/csharp_interviews/csharp_interviews/exos/MathParser.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace csharp_interviews.exos
+{
+    public class MathParser
+    {
+        public int Parse(string input)
+        {
+            while (input.Contains("*"))
+            {
+                var match = new Regex(@"\d \* \d").Match(input);
+                if (match.Success)
+                {
+                    var value = match.Value;
+                    var sides = value.Split(new char[] { ' ', '*' }).Where(x => !string.IsNullOrEmpty(x)).ToArray();
+                    int intValue = int.Parse(sides[0]) * int.Parse(sides[1]);
+                    input = input.Replace(value, intValue.ToString());
+                }
+                else
+                    break;
+            }
+
+            return input.Replace("- ", "+ -").Split('+').Select(x => int.Parse(x)).Sum();
+        }
+    }
+}
---
+++ tr/csharp_interviews/ut/MathParserTestClass.cs
@@ -0,0 +1,41 @@
+﻿using csharp_interviews.exos;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace ut
+{
+    [TestFixture]
+    public class MathParserTestClass
+    {
+        MathParser MathParser;
+
+        [SetUp]
+        public void Setup() 
+        {
+            MathParser = new MathParser();
+        }
+
+        [TestCase("2 * 3", 6)]
+        [TestCase("8 + 4", 12)]
+        [TestCase("4 * 8", 32)]
+        [TestCase("8 - 2", 6)]
+        [TestCase("2 * 3 * 8", 48)]
+
+        public void UniOperator(string input, int expected)
+        {
+            int result = MathParser.Parse(input);
+            Assert.AreEqual(expected, result);
+        }
+        
+        [TestCase("3 + 6 - 9 + 5 + 6 * 8 + 12 + 24 - 6 + 6 * 3 + 8 + 1 * 2 * 3", 115)]
+        public void MultiOperator(string input, int expected)
+        {
+            int result = MathParser.Parse(input);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
