10565bb61587c8bac6a894ab437de6c5fdabe250
Houssam FERTAQ <fertaqhoussam@hotmail.com>
2020-04-12 11:51:01 +0200

    Added deletion tests

---
+++ tr/tr2/XUnitTestProject/UnitTests/Commands/DeleteOrderDetailCommandTests.cs
@@ -0,0 +1,33 @@
+﻿using app.Operations.ProductOrders.Commands.DeleteOrderDetail;
+using domain.Entities;
+using NUnitTestProject;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using Xunit;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace XUnitTestProject.UnitTests.Commands
+{
+    [Collection("Queries")]
+    public class DeleteOrderDetailCommandTests
+    {
+        private readonly NorthwindContext _context;
+        public DeleteOrderDetailCommandTests(QueriesServiceCollection queriesServiceCollection)
+        {
+            _context = queriesServiceCollection.context;
+        }
+
+        [Fact]
+        public async Task DeleteOrderDetailCommand_Ok() 
+        {
+            var command = new DeleteOrderDetailCommand() { OrderID = 111036, ProductID = 59 };
+            var commandExcution = new DeleteOrderDetailCommand.DeleteOrderDetailCommandHandler(_context).Handle(command, CancellationToken.None);
+            var entityExists = await _context.OrderDetails.AnyAsync(x => x.OrderId == command.OrderID && x.ProductId == command.ProductID);
+            Assert.True(!entityExists);
+        }
+    }
+}
