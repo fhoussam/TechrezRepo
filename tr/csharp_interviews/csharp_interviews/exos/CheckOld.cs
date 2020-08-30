27836759a072024fb66117370031c02b15d1c72c
Houssam FERTAQ <fertaqhoussam@hotmail.com>
2020-08-30 12:21:17 +0200

    refacto of check exo

---
+++ tr/csharp_interviews/csharp_interviews/exos/CheckOld.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace csharp_interviews.exos
+{
+    public class CheckOld
+    {
+        public static void CoreFunction()
+        {
+            bool result = CheckAll("[](()()[[]])()[]([])");
+            Console.WriteLine(result);
+        }
+
+        private static bool CheckAll(string str) 
+        {
+            string strWithBigBrackets = new string(str.Where(x => x == '[' || x == ']').ToArray());
+            string strWithSmallBrackets = new string(str.Where(x => x == '(' || x == ')').ToArray());
+            bool checkSmallBrackets = CheckBrakets(strWithSmallBrackets);
+            if (checkSmallBrackets)
+            {
+                return CheckBrakets(strWithBigBrackets);
+            }
+            else
+                return false;
+        }
+
+        private static bool CheckBrakets(string str)
+        {
+            bool isBigBracket = str.All(x => x == '[' || x == ']');
+            char openingBracket = isBigBracket ? '[' : '(';
+            char closingBracket = isBigBracket ? ']' : ')';
+            string stringToProcess = new string(str.Where(x => x == openingBracket || x == closingBracket).ToArray());
+
+            while (!string.IsNullOrEmpty(stringToProcess))
+            {
+                try
+                {
+                    Shrink(ref stringToProcess, openingBracket, closingBracket);
+                }
+                catch (MyCustomException)
+                {
+                    return false;
+                }
+            }
+
+            return string.IsNullOrEmpty(stringToProcess);
+        }
+
+        private static void Shrink(ref string str, char openingBracket, char closingBracket) 
+        {
+            var firstClosingIndex = str.IndexOf(closingBracket);
+            var firstOpeningIndex = str.IndexOf(openingBracket);
+            if (firstClosingIndex == -1 || firstOpeningIndex == -1)
+                throw new MyCustomException();
+            else
+            {
+                str = RemoveCharAt(str, firstClosingIndex);
+                str = RemoveCharAt(str, firstOpeningIndex);
+            }
+        }
+
+        private static string RemoveCharAt(string str, int index) 
+        {
+            var aa = str.ToList();
+            aa.RemoveAt(index);
+            return new string(aa.ToArray());
+        }
+    }
+
+    public class MyCustomException : Exception{ }
+}
---
+++ tr/csharp_interviews/ut/exos/Check.cs
@@ -0,0 +1,21 @@
+﻿using csharp_interviews.exos;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ut.exos
+{
+    public class CheckValidString
+    {
+        [Fact]
+        public void ThenShouldReturnTrue() 
+        {
+            var expected = true;
+            var input = "[](()()[[]])()[]([])";
+            var actual = Check.ProcessBrackets(input);
+            actual.Should().Be(expected);
+        }
+    }
+}
