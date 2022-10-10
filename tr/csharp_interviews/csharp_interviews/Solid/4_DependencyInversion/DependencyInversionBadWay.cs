87b0ca0f2633551d410fcd8ea98b2e1dc1264457
fhoussam <houssamfertaq@gmail.com>
2022-10-10 22:37:09 +0200

    solid : Dependency Inversion

---
+++ tr/csharp_interviews/csharp_interviews/Solid/4_DependencyInversion/DependencyInversionBadWay.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace csharp_interviews.Solid._4_DependencyInversion
+{
+    internal class DependencyInversionBadWay
+    {
+        public class MyFeatureThatUsesApi 
+        {
+            public void DoSomethingInteresting() 
+            {
+                var myApi = new MyFeatureThatUsesApi();
+                myApi.DoSomethingInteresting();
+            }
+        }
+
+        public class MyExternalUsefulApi 
+        {
+            public void DoSomethingInteresting() { }
+            public void DoSomethingIDontReallyNeed() { }
+        }
+    }
+}
---
+++ tr/csharp_interviews/csharp_interviews/Solid/4_DependencyInversion/DependencyInversionRightWay.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace csharp_interviews.Solid._4_DependencyInversion
+{
+    //higher level layer should only depend on lower level by using abstractions
+    internal class DependencyInversionRightWay
+    {
+        public class MyFeatureThatUsesApi
+        {
+            private readonly IExternalUsefulApi _externalUsefulApi;
+
+            public MyFeatureThatUsesApi(IExternalUsefulApi externalUsefulApi)
+            {
+                this._externalUsefulApi = externalUsefulApi;
+            }
+
+            public void DoSomethingInteresting()
+            {
+                _externalUsefulApi.DoSomethingInteresting();
+            }
+        }
+
+        public interface IExternalUsefulApi 
+        {
+            public void DoSomethingInteresting();
+        }
+
+        public class MyExternalUsefulApi : IExternalUsefulApi
+        {
+            public void DoSomethingInteresting() { }
+            public void DoSomethingIDontReallyNeed() { }
+        }
+    }
+}
