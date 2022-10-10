bad786ef058e7b1c369470c66f57e618f54dab3e
fhoussam <houssamfertaq@gmail.com>
2022-10-10 21:39:31 +0200

    solid : Interface Segregation

---
+++ tr/csharp_interviews/csharp_interviews/Solid/3_InterfaceSegregation/InterfaceSegregationBadWay.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace csharp_interviews.Solid._3_InterfaceSegregation
+{
+    internal class InterfaceSegregationBadWay
+    {
+        public class Document { }
+
+        public interface IMachine
+        {
+            public void Print();
+            public void Scan();
+            public void Fax();
+        }
+
+        public class MultiFunctionPrinter : IMachine
+        {
+            public void Fax()
+            {
+                // i can fax
+            }
+
+            public void Print()
+            {
+                // i can print
+            }
+
+            public void Scan()
+            {
+                // i can Scan
+            }
+        }
+
+        public class OldFashionedPrinter : IMachine
+        {
+            public void Fax()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Print()
+            {
+                // i can only print :(
+            }
+
+            public void Scan()
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
---
+++ tr/csharp_interviews/csharp_interviews/Solid/3_InterfaceSegregation/InterfaceSegregationRightWay.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace csharp_interviews.Solid._3_InterfaceSegregation
+{
+    public class Document { }
+
+    public interface IPrinter 
+    { 
+        void Print(Document document);
+    }
+
+    public interface IScanner 
+    {
+        void Scan(Document document);
+    }
+
+    public interface IMultifunctionDevice  : IPrinter, IScanner
+    {
+
+    }
+
+    public class MultifunctionDevice_1 : IMultifunctionDevice
+    {
+        public void Print(Document document)
+        {
+            //print
+        }
+
+        public void Scan(Document document)
+        {
+            //scan
+        }
+    }
+
+    //OR
+    public class MultifunctionDevice_2
+    {
+        private readonly IPrinter printer;
+        private readonly IScanner scanner;
+
+        public MultifunctionDevice_2(IPrinter printer, IScanner scanner)
+        {
+            this.printer = printer;
+            this.scanner = scanner;
+        }
+
+        public void Print(Document document)
+        {
+            printer.Print(document);
+        }
+
+        public void Scan(Document document)
+        {
+            scanner.Scan(document);
+        }
+    }
+}
