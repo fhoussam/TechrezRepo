7bb59a40c3e1b123b08280ffd97bf2a714f3794e
fhoussam <houssamfertaq@gmail.com>
2022-10-11 14:14:56 +0200

    parition by

---
+++ tr/csharp_interviews/csharp_interviews/SQL/partition_by.sql
@@ -0,0 +1,8 @@
+﻿select 
+	p.ProductID, 
+	p.ProductName, 
+	c.CategoryName,
+	--counting cateogries for each row
+	count(c.CategoryID) over (partition by c.CategoryID) as TotalCategories
+from Products p
+join Categories c on p.CategoryID = c.CategoryID
